eab55b0ff23bd18d4d2d53a4b6914d4683a4dd40
dilyar111 <231274@astanait.edu.kz>
2025-05-19 01:35:34 +0500

    email

--- email-service/cmd/main.go
+++ email-service/cmd/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	to := "dilara.galimkizi@gmail.com"
 	subject := "from Dilyara"
-	body := "Hello from Go Microservice! Aruzhan Ali we love you!"
+	body := "Hello from Go Microservice! Dear Aruzhan Ali, Thank you for being the kindest and most wonderful teacher. Your support, patience, and encouragement meant so much. I truly appreciate all your hard work and dedication. Wishing you all the best! Sincerely, SE-2310"
 
 	err = email.SendEmail(to, subject, body)
 	if err != nil {
